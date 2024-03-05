d270ebea4374db16cfd6abcc6ac8ad90238b9ae4
kaeraeaa <129053421+kaeraeaa@users.noreply.github.com>
2024-03-05 14:41:47 +0200

    comit

---
+++ script_1.js
@@ -0,0 +1,45 @@
+// Завдання 1
+
+const friends = ['mango', 'poly', 'kiwi', 'ajax'];
+let string = '';
+
+for (let i = 0; i < friends.length; i++) {
+  if (i === friends.length - 1) {
+    string += friends[i];
+  } else {
+    string += friends[i] + ', ';
+  }
+}
+
+// const friends = ['mango', 'poly', 'kiwi', 'ajax'];
+// let string = friends.join(', ');
+
+console.log(string);
+
+
+// Завдання 2
+
+const cards = [
+    'Карточка-1',
+    'Карточка-2',
+    'Карточка-3',
+    'Карточка-4',
+    'Карточка-5',
+]
+
+const cardToRemove = 'Карточка-3';
+cards.splice(2, 1);
+
+const cardToInset = 'Карточка-6';
+cards.splice(5, 5,  'Карточка-6');
+
+const cardToUpdate = 'Карточка-4';
+cards.splice(2, 1, 'Нова карта');
+
+console.log(cards);
+
+// Завдання 3
+
+// Завдання 4
+
+// Завдання 5
